0370021f732fce307556e66dd35bc02c270ba0ac
Shane Mc Cormack <dataforce@dataforce.org.uk>
2015-09-08 14:50:34 +0100

    Spaces to Tabs

--- scripts/build-tinc-config.sh
+++ scripts/build-tinc-config.sh
@@ -13,175 +13,175 @@ SHELL_API=`which cli-shell-api`
 MYSESSION="false"
 $SHELL_API inSession
 if [ $? -ne 0 ]; then
-  # Obtain session environment
-  session_env=$($SHELL_API getSessionEnv $PPID)
+	# Obtain session environment
+	session_env=$($SHELL_API getSessionEnv $PPID)
 
-  # Evaluate environment string
-  eval $session_env
+	# Evaluate environment string
+	eval $session_env
 
-  # Setup the session
-  $SHELL_API setupSession
-  $SHELL_API inSession
-  if [ $? -ne 0 ]; then
-    echo "Something went wrong setting up session."
-    exit 1;
-  fi
-  MYSESSION="true"
+	# Setup the session
+	$SHELL_API setupSession
+	$SHELL_API inSession
+	if [ $? -ne 0 ]; then
+		echo "Something went wrong setting up session."
+		exit 1;
+	fi
+	MYSESSION="true"
 
-  function atexit() {
-    $SHELL_API teardownSession
-  }
-  trap atexit EXIT
+	function atexit() {
+		$SHELL_API teardownSession
+	}
+	trap atexit EXIT
 fi;
 
 
 net_list=$($SHELL_API listNodes protocols tinc)
 eval "NETS=($net_list)"
 for net in "${NETS[@]}"; do
-  mkdir -p /etc/tinc/$net/hosts;
+	mkdir -p /etc/tinc/$net/hosts;
 
-  config_list=$($SHELL_API listNodes protocols tinc $net)
-  eval "CONFIG_LIST=($config_list)"
-  for cfg in "${CONFIG_LIST[@]}"; do
-    if [ "${cfg}" = "host" ]; then
-      host_list=$($SHELL_API listNodes protocols tinc $net host)
-      eval "HOST_LIST=($host_list)"
+	config_list=$($SHELL_API listNodes protocols tinc $net)
+	eval "CONFIG_LIST=($config_list)"
+	for cfg in "${CONFIG_LIST[@]}"; do
+		if [ "${cfg}" = "host" ]; then
+			host_list=$($SHELL_API listNodes protocols tinc $net host)
+			eval "HOST_LIST=($host_list)"
 
-      for host in "${HOST_LIST[@]}"; do
-        HOSTPUBKEY=""
-        host_config_list=$($SHELL_API listNodes protocols tinc $net host $host)
-        eval "HOST_CONFIG_LIST=($host_config_list)"
-        for hcfg in "${HOST_CONFIG_LIST[@]}"; do
-          if [ "${hcfg}" = "publickey" ]; then
-            HOSTPUBKEY=$($SHELL_API returnValue protocols tinc $net host $host $hcfg)
-          else
-            FOUND=0
-            values=$($SHELL_API returnValues protocols tinc $net host $host $hcfg)
-            eval "VALUES=($values)";
-            for val in "${VALUES[@]}"; do
-              if [ "${val}" != "" ]; then
-                echo "${hcfg} ${val}" >> /etc/tinc/$net/hosts/$host
-              fi;
-              FOUND=1
-            done;
+			for host in "${HOST_LIST[@]}"; do
+				HOSTPUBKEY=""
+				host_config_list=$($SHELL_API listNodes protocols tinc $net host $host)
+				eval "HOST_CONFIG_LIST=($host_config_list)"
+				for hcfg in "${HOST_CONFIG_LIST[@]}"; do
+					if [ "${hcfg}" = "publickey" ]; then
+						HOSTPUBKEY=$($SHELL_API returnValue protocols tinc $net host $host $hcfg)
+					else
+						FOUND=0
+						values=$($SHELL_API returnValues protocols tinc $net host $host $hcfg)
+						eval "VALUES=($values)";
+						for val in "${VALUES[@]}"; do
+							if [ "${val}" != "" ]; then
+								echo "${hcfg} ${val}" >> /etc/tinc/$net/hosts/$host
+							fi;
+							FOUND=1
+						done;
 
-            if [ "${FOUND}" = "0" ]; then
-              val=$($SHELL_API returnValue protocols tinc $net host $host $hcfg)
-              if [ "${val}" != "" ]; then
-                echo "${hcfg} ${val}" >> /etc/tinc/$net/hosts/$host
-              fi;
-            fi;
-          fi;
-        done;
-        if [ "${HOSTPUBKEY}" != "" ]; then
-          echo '' >> /etc/tinc/$net/hosts/$host
-          echo '-----BEGIN RSA PUBLIC KEY-----' >> /etc/tinc/$net/hosts/$host
-          echo "${HOSTPUBKEY}" | sed -r 's/(.{65})/\1\n/g' >> /etc/tinc/$net/hosts/$host
-          echo '-----END RSA PUBLIC KEY-----' >> /etc/tinc/$net/hosts/$host
-        fi;
-      done;
-    elif [ "${cfg}" = "proxy" ]; then
-      # TODO: Proxy stuff.
-      echo -n ""
-    elif [ "${cfg}" = "tinc-up" -o "${cfg}" = "tinc-down" ]; then
-      val=$($SHELL_API returnValue protocols tinc $net $cfg)
-      if [ -e "${val}" ]; then
-        ln -s ${val} /etc/tinc/$net/$cfg
-      fi;
-    elif [ "${cfg}" = "myip" -o "${cfg}" = "enabled" ]; then
-      # Do nothing. This is a vyatta-tinc config setting, not a tinc config setting.
-      echo -n ""
-    elif [ "${cfg}" = "privatekey" ]; then
-      PRIVATEKEY=$($SHELL_API returnValue protocols tinc $net privatekey)
-      if [ "${PRIVATEKEY}" != "" ]; then
-        echo '-----BEGIN RSA PRIVATE KEY-----' >> /etc/tinc/$net/rsa_key.priv
-        echo "${PRIVATEKEY}" | sed -r 's/(.{65})/\1\n/g' >> /etc/tinc/$net/rsa_key.priv
-        echo '-----END RSA PRIVATE KEY-----' >> /etc/tinc/$net/rsa_key.priv
-        chmod 600 /etc/tinc/$net/rsa_key.priv
-      fi;
-    elif [ "${cfg}" = "generatekeys" ]; then
-      GENERATE=$($SHELL_API returnValue protocols tinc $net generatekeys)
-      PRIVATEKEY=$($SHELL_API returnValue protocols tinc $net privatekey)
-      if [ "${GENERATE}" = "true" -a "${PRIVATEKEY}" = "" -a -e "/usr/sbin/tincd" ]; then
-        MYNAME=$($SHELL_API returnValue protocols tinc $net name)
-        if [ "${MYNAME}" != "" ]; then
-          TEMPDIR=`mktemp -d`
-          echo "" | /usr/sbin/tincd -K -c "${TEMPDIR}" 2>/dev/null
-          MYPUBLICKEY=$(cat "${TEMPDIR}/rsa_key.pub" | egrep -v "^(.*(BEGIN|END) RSA.*|)$" | tr -d '\n')
-          MYPRIVATEKEY=$(cat "${TEMPDIR}/rsa_key.priv" | egrep -v "^(.*(BEGIN|END) RSA.*|)$" | tr -d '\n')
-          /opt/vyatta/sbin/my_set protocols tinc $net privatekey "${MYPRIVATEKEY}"
-          /opt/vyatta/sbin/my_set protocols tinc $net host ${MYNAME} publickey "${MYPUBLICKEY}"
-          if [ "${MYSESSION}" = "true" ]; then
-            /opt/vyatta/sbin/my_commit 2>&1
-            exit $?
-          fi;
-          
-          cp "${TEMPDIR}/rsa_key.priv" /etc/tinc/${net}/rsa_key.priv
-          if [ -e "/etc/tinc/${net}/hosts/${MYNAME}" ]; then
-            cat "${TEMPDIR}/rsa_key.pub" >> "/etc/tinc/${net}/hosts/${MYNAME}"
-          fi;
-          rm -Rf "${TEMPDIR}"
-        fi;
-      fi;
-    else
-      FOUND=0
-      values=$($SHELL_API returnValues protocols tinc $net $cfg)
-      eval "VALUES=($values)";
-      for val in "${VALUES[@]}"; do
-        if [ "${val}" != "" ]; then
-          echo "${cfg} ${val}" >> /etc/tinc/$net/tinc.conf
-        fi;
-        FOUND=1
-      done;
+						if [ "${FOUND}" = "0" ]; then
+							val=$($SHELL_API returnValue protocols tinc $net host $host $hcfg)
+							if [ "${val}" != "" ]; then
+								echo "${hcfg} ${val}" >> /etc/tinc/$net/hosts/$host
+							fi;
+						fi;
+					fi;
+				done;
+				if [ "${HOSTPUBKEY}" != "" ]; then
+					echo '' >> /etc/tinc/$net/hosts/$host
+					echo '-----BEGIN RSA PUBLIC KEY-----' >> /etc/tinc/$net/hosts/$host
+					echo "${HOSTPUBKEY}" | sed -r 's/(.{65})/\1\n/g' >> /etc/tinc/$net/hosts/$host
+					echo '-----END RSA PUBLIC KEY-----' >> /etc/tinc/$net/hosts/$host
+				fi;
+			done;
+		elif [ "${cfg}" = "proxy" ]; then
+			# TODO: Proxy stuff.
+			echo -n ""
+		elif [ "${cfg}" = "tinc-up" -o "${cfg}" = "tinc-down" ]; then
+			val=$($SHELL_API returnValue protocols tinc $net $cfg)
+			if [ -e "${val}" ]; then
+				ln -s ${val} /etc/tinc/$net/$cfg
+			fi;
+		elif [ "${cfg}" = "myip" -o "${cfg}" = "enabled" ]; then
+			# Do nothing. This is a vyatta-tinc config setting, not a tinc config setting.
+			echo -n ""
+		elif [ "${cfg}" = "privatekey" ]; then
+			PRIVATEKEY=$($SHELL_API returnValue protocols tinc $net privatekey)
+			if [ "${PRIVATEKEY}" != "" ]; then
+				echo '-----BEGIN RSA PRIVATE KEY-----' >> /etc/tinc/$net/rsa_key.priv
+				echo "${PRIVATEKEY}" | sed -r 's/(.{65})/\1\n/g' >> /etc/tinc/$net/rsa_key.priv
+				echo '-----END RSA PRIVATE KEY-----' >> /etc/tinc/$net/rsa_key.priv
+				chmod 600 /etc/tinc/$net/rsa_key.priv
+			fi;
+		elif [ "${cfg}" = "generatekeys" ]; then
+			GENERATE=$($SHELL_API returnValue protocols tinc $net generatekeys)
+			PRIVATEKEY=$($SHELL_API returnValue protocols tinc $net privatekey)
+			if [ "${GENERATE}" = "true" -a "${PRIVATEKEY}" = "" -a -e "/usr/sbin/tincd" ]; then
+				MYNAME=$($SHELL_API returnValue protocols tinc $net name)
+				if [ "${MYNAME}" != "" ]; then
+					TEMPDIR=`mktemp -d`
+					echo "" | /usr/sbin/tincd -K -c "${TEMPDIR}" 2>/dev/null
+					MYPUBLICKEY=$(cat "${TEMPDIR}/rsa_key.pub" | egrep -v "^(.*(BEGIN|END) RSA.*|)$" | tr -d '\n')
+					MYPRIVATEKEY=$(cat "${TEMPDIR}/rsa_key.priv" | egrep -v "^(.*(BEGIN|END) RSA.*|)$" | tr -d '\n')
+					/opt/vyatta/sbin/my_set protocols tinc $net privatekey "${MYPRIVATEKEY}"
+					/opt/vyatta/sbin/my_set protocols tinc $net host ${MYNAME} publickey "${MYPUBLICKEY}"
+					if [ "${MYSESSION}" = "true" ]; then
+						/opt/vyatta/sbin/my_commit 2>&1
+						exit $?
+					fi;
 
-      if [ "${FOUND}" = "0" ]; then
-        val=$($SHELL_API returnValue protocols tinc $net $cfg)
-        if [ "${val}" != "" ]; then
-          echo "${cfg} ${val}" >> /etc/tinc/$net/tinc.conf
-        fi;
-      fi;
-    fi;
-  done;
+					cp "${TEMPDIR}/rsa_key.priv" /etc/tinc/${net}/rsa_key.priv
+					if [ -e "/etc/tinc/${net}/hosts/${MYNAME}" ]; then
+						cat "${TEMPDIR}/rsa_key.pub" >> "/etc/tinc/${net}/hosts/${MYNAME}"
+					fi;
+					rm -Rf "${TEMPDIR}"
+				fi;
+			fi;
+		else
+			FOUND=0
+			values=$($SHELL_API returnValues protocols tinc $net $cfg)
+			eval "VALUES=($values)";
+			for val in "${VALUES[@]}"; do
+				if [ "${val}" != "" ]; then
+					echo "${cfg} ${val}" >> /etc/tinc/$net/tinc.conf
+				fi;
+				FOUND=1
+			done;
 
-  if [ ! -e /etc/tinc/$net/tinc-up ]; then
-    echo '#!/bin/sh' > /etc/tinc/$net/tinc-up
-    echo 'ip link set $INTERFACE up' >> /etc/tinc/$net/tinc-up
+			if [ "${FOUND}" = "0" ]; then
+				val=$($SHELL_API returnValue protocols tinc $net $cfg)
+				if [ "${val}" != "" ]; then
+					echo "${cfg} ${val}" >> /etc/tinc/$net/tinc.conf
+				fi;
+			fi;
+		fi;
+	done;
 
-    values=$($SHELL_API returnValues protocols tinc $net myip)
-    eval "VALUES=($values)";
-    for val in "${VALUES[@]}"; do
-      if [ "${val}" != "" ]; then
-        echo "ip addr add ${val} dev \$INTERFACE" >> /etc/tinc/$net/tinc-up
-      fi;
-    done;
+	if [ ! -e /etc/tinc/$net/tinc-up ]; then
+		echo '#!/bin/sh' > /etc/tinc/$net/tinc-up
+		echo 'ip link set $INTERFACE up' >> /etc/tinc/$net/tinc-up
 
-    chmod a+x /etc/tinc/$net/tinc-up
-  fi;
-     
-  NET_ENABLED=$($SHELL_API returnValue protocols tinc $net enabled)
+		values=$($SHELL_API returnValues protocols tinc $net myip)
+		eval "VALUES=($values)";
+		for val in "${VALUES[@]}"; do
+			if [ "${val}" != "" ]; then
+				echo "ip addr add ${val} dev \$INTERFACE" >> /etc/tinc/$net/tinc-up
+			fi;
+		done;
 
-  if [ "${NET_ENABLED}" = "true" ]; then
-    echo $net >> /etc/tinc/nets.boot
-  else
-    rm -Rf /etc/tinc/$net/
-  fi;
+		chmod a+x /etc/tinc/$net/tinc-up
+	fi;
+
+	NET_ENABLED=$($SHELL_API returnValue protocols tinc $net enabled)
+
+	if [ "${NET_ENABLED}" = "true" ]; then
+		echo $net >> /etc/tinc/nets.boot
+	else
+		rm -Rf /etc/tinc/$net/
+	fi;
 done;
 
 # If tinc is installed, then poke it a bit.
 if [ -e /etc/init.d/tinc ]; then
-  # Make sure tinc is running for all networks
-  /etc/init.d/tinc start >/dev/null 2>&1
+	# Make sure tinc is running for all networks
+	/etc/init.d/tinc start >/dev/null 2>&1
 
-  # Reload all networks.
-  /etc/init.d/tinc reload >/dev/null 2>&1
+	# Reload all networks.
+	/etc/init.d/tinc reload >/dev/null 2>&1
 fi;
 
 # Kill networks that no longer exist.
 for TINCPID in `ls /var/run/tinc.*.pid 2>/dev/null`; do
-  TINCNET=`echo ${TINCPID} | sed -r 's/.*tinc\.(.*)\.pid/\1/'`
-  
-  if [ ! -e "/etc/tinc/${TINCNET}/" ]; then
-    kill -TERM $(cat ${TINCPID}) >/dev/null 2>&1
-    rm ${TINCPID};
-  fi;
+	TINCNET=`echo ${TINCPID} | sed -r 's/.*tinc\.(.*)\.pid/\1/'`
+
+	if [ ! -e "/etc/tinc/${TINCNET}/" ]; then
+		kill -TERM $(cat ${TINCPID}) >/dev/null 2>&1
+		rm ${TINCPID};
+	fi;
 done;
--- scripts/tinc-commands.sh
+++ scripts/tinc-commands.sh
@@ -5,96 +5,96 @@ SHELL_API=`which cli-shell-api`
 # Ensure we have a session.
 $SHELL_API inSession
 if [ $? -ne 0 ]; then
-  # Obtain session environment
-  session_env=$($SHELL_API getSessionEnv $PPID)
+	# Obtain session environment
+	session_env=$($SHELL_API getSessionEnv $PPID)
 
-  # Evaluate environment string
-  eval $session_env
+	# Evaluate environment string
+	eval $session_env
 
-  # Setup the session
-  $SHELL_API setupSession
-  $SHELL_API inSession
-  if [ $? -ne 0 ]; then
-    echo "Something went wrong setting up session."
-    exit 1;
-  fi
+	# Setup the session
+	$SHELL_API setupSession
+	$SHELL_API inSession
+	if [ $? -ne 0 ]; then
+		echo "Something went wrong setting up session."
+		exit 1;
+	fi
 
-  function atexit() {
-    $SHELL_API teardownSession
-  }
-  trap atexit EXIT
+	function atexit() {
+		$SHELL_API teardownSession
+	}
+	trap atexit EXIT
 fi;
 
 if [ "${1}" = "networklist" ]; then
-  net_list=$($SHELL_API listNodes protocols tinc)
-  eval "NETS=($net_list)"
-  for net in "${NETS[@]}"; do  
-    echo $net
-  done;
+	net_list=$($SHELL_API listNodes protocols tinc)
+	eval "NETS=($net_list)"
+	for net in "${NETS[@]}"; do
+		echo $net
+	done;
 elif [ "${1}" = "status" ]; then
-  if [ ! -e "/etc/init.d/tinc" ]; then
-    echo "WARNING: tinc is not installed."
-  fi;
+	if [ ! -e "/etc/init.d/tinc" ]; then
+		echo "WARNING: tinc is not installed."
+	fi;
 
-  net_list=$($SHELL_API listNodes protocols tinc)
-  eval "NETS=($net_list)"
-  for net in "${NETS[@]}"; do  
-    NET_ENABLED=$($SHELL_API returnValue protocols tinc $net enabled)
+	net_list=$($SHELL_API listNodes protocols tinc)
+	eval "NETS=($net_list)"
+	for net in "${NETS[@]}"; do
+		NET_ENABLED=$($SHELL_API returnValue protocols tinc $net enabled)
 
-    if [ "${NET_ENABLED}" = "true" ]; then
-      echo -n "$net: "
-      if [ -e "/var/run/tinc.${net}.pid" ]; then
-        echo "Running ($(cat "/var/run/tinc.${net}.pid"))";
-      else
-        echo "Not running";
-      fi;
-    else
-      echo "$net: Disabled"
-    fi;
-  done;
+		if [ "${NET_ENABLED}" = "true" ]; then
+			echo -n "$net: "
+			if [ -e "/var/run/tinc.${net}.pid" ]; then
+				echo "Running ($(cat "/var/run/tinc.${net}.pid"))";
+			else
+				echo "Not running";
+			fi;
+		else
+			echo "$net: Disabled"
+		fi;
+	done;
 elif [ "${1}" = "connections" -o "${1}" = "statistics" ]; then
-  net="${2}"
-  if [ "${1}" = "connections" ]; then
-    SIGNAL="USR1"
-  elif [ "${1}" = "statistics" ]; then
-    SIGNAL="USR2"
-  fi;
+	net="${2}"
+	if [ "${1}" = "connections" ]; then
+		SIGNAL="USR1"
+	elif [ "${1}" = "statistics" ]; then
+		SIGNAL="USR2"
+	fi;
 
-  # Make sure syslog will log what we need for the show commands.
-  if [ ! -e "/etc/rsyslog.d/vyatta-tinc.conf" -a -e "/etc/rsyslog.d/" ]; then
-    echo ':syslogtag, startswith, "tinc."   -/var/log/messages' > /etc/rsyslog.d/vyatta-tinc.conf
-    /etc/init.d/rsyslog restart >/dev/null 2>&1
-  fi;
+	# Make sure syslog will log what we need for the show commands.
+	if [ ! -e "/etc/rsyslog.d/vyatta-tinc.conf" -a -e "/etc/rsyslog.d/" ]; then
+		echo ':syslogtag, startswith, "tinc."   -/var/log/messages' > /etc/rsyslog.d/vyatta-tinc.conf
+		/etc/init.d/rsyslog restart >/dev/null 2>&1
+	fi;
 
-  if [ "${net}" != "" ]; then
-    if [ -e "/var/run/tinc.${net}.pid" ]; then
-      echo "Attempting to get ${1} information from tinc: "
-      kill -${SIGNAL} $(cat "/var/run/tinc.${net}.pid") 2>&1
+	if [ "${net}" != "" ]; then
+		if [ -e "/var/run/tinc.${net}.pid" ]; then
+			echo "Attempting to get ${1} information from tinc: "
+			kill -${SIGNAL} $(cat "/var/run/tinc.${net}.pid") 2>&1
 
-      cat /var/log/messages | grep "tinc.${net}\[$(cat "/var/run/tinc.${net}.pid")\]" | grep `date +%H:%M:%S` | cut -f 4- -d:
-    else
-      echo "Unknown tinc instance, or tinc instance not currently running: ${net}"
-    fi;
-  else
-    echo "No tinc instance specified."
-  fi;
+			cat /var/log/messages | grep "tinc.${net}\[$(cat "/var/run/tinc.${net}.pid")\]" | grep `date +%H:%M:%S` | cut -f 4- -d:
+		else
+			echo "Unknown tinc instance, or tinc instance not currently running: ${net}"
+		fi;
+	else
+		echo "No tinc instance specified."
+	fi;
 elif [ "${1}" = "logging" ]; then
-  net="${2}"
-  if [ "${net}" != "" ]; then
-    if [ "${3}" = "all" ]; then
-      echo "All log entries:"
-      cat /var/log/messages | grep "tinc.${net}" | tail -n 50
-    else
-      if [ -e "/var/run/tinc.${net}.pid" ]; then
-        echo "Recent log entries:"
-        cat /var/log/messages | grep "tinc.${net}\[$(cat "/var/run/tinc.${net}.pid")\]" | tail -n 50
-      else
-        echo "Unknown tinc instance, or tinc instance not currently running: ${net}"
-      fi;
-    fi;
-  else
-    echo "No tinc instance specified."
-  fi;
+	net="${2}"
+	if [ "${net}" != "" ]; then
+		if [ "${3}" = "all" ]; then
+			echo "All log entries:"
+			cat /var/log/messages | grep "tinc.${net}" | tail -n 50
+		else
+			if [ -e "/var/run/tinc.${net}.pid" ]; then
+				echo "Recent log entries:"
+				cat /var/log/messages | grep "tinc.${net}\[$(cat "/var/run/tinc.${net}.pid")\]" | tail -n 50
+			else
+				echo "Unknown tinc instance, or tinc instance not currently running: ${net}"
+			fi;
+		fi;
+	else
+		echo "No tinc instance specified."
+	fi;
 else
-  echo "Unknown command."
+	echo "Unknown command."
 fi;
